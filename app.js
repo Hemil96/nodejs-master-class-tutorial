6cde722cfca1b44b5ea4d7e377595d03cbee4ded
Hemil Patel <hemilpatel41196@gmail.com>
2018-10-12 03:37:08 -0700

    Payload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Dependencies  
 const http = require('http');
 const url = require('url');
+let StringDecoder = require('string_decoder').StringDecoder;
 
 // Creating a server that will return a string on requests
 const server = http.createServer((req, res) => {
@@ -12,20 +13,32 @@ const server = http.createServer((req, res) => {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-  //  Get the query string as an object
+  // Get the query string as an object
   const queryStringObject = parsedUrl.query; 
 
   // Get the http method
   const method = req.method.toLowerCase(); 
 
-  // Get the headers as a json object
+  // Get the headers as an object
   const headers = req.headers
+
+  // Get the payload, if any 
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  // Collect data from stream  
+  req.on('data', (data) => {
+    buffer += decoder.write(data )
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
 
   // Send the response 
   res.end('Hello World!\n');
 
-  // Log the request path
-  console.log('This request is sent with these headers', headers);
+  // Log the request payload
+  console.log('This request is sent with these payload', buffer);
+  })
 });
 
 // Listing to the server
